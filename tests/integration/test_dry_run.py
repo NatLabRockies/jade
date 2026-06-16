daebae9212975c4b31150a2a763785cbf11ef1d7
Daniel Thom <daniel.thom@nlr.gov>
2026-06-16 07:08:27 -0600

    Fix flaky tests

--- tests/integration/test_dry_run.py
+++ tests/integration/test_dry_run.py
@@ -1,45 +1,26 @@
 """Unit tests for dry-run mode"""
 
-import os
-import shutil
-
-import pytest
-
 from jade.extensions.generic_command import GenericCommandInputs
 from jade.extensions.generic_command import GenericCommandConfiguration
 from jade.test_common import SLURM_HPC_CONFIG
 from jade.utils.run_command import check_run_command
 
 
-TEST_FILENAME = "test-inputs.txt"
-CONFIG_FILE = "test-config.json"
-OUTPUT = "test-output"
 NUM_COMMANDS = 5
 
 
-@pytest.fixture
-def cleanup():
-    _do_cleanup()
+def test_dry_run(tmp_path):
     commands = ['echo "hello world"'] * NUM_COMMANDS
-    with open(TEST_FILENAME, "w") as f_out:
+    inputs_file = tmp_path / "test-inputs.txt"
+    with open(inputs_file, "w") as f_out:
         for command in commands:
             f_out.write(command + "\n")
 
-    inputs = GenericCommandInputs(TEST_FILENAME)
+    inputs = GenericCommandInputs(str(inputs_file))
     config = GenericCommandConfiguration.auto_config(inputs)
-    config.dump(CONFIG_FILE)
-    yield
-    _do_cleanup()
+    config_file = tmp_path / "test-config.json"
+    config.dump(str(config_file))
 
-
-def _do_cleanup():
-    for path in (TEST_FILENAME, CONFIG_FILE, OUTPUT):
-        if os.path.isdir(path):
-            shutil.rmtree(path)
-        elif os.path.exists(path):
-            os.remove(path)
-
-
-def test_dry_run(cleanup):
-    cmd = f"jade submit-jobs --dry-run -h {SLURM_HPC_CONFIG} {CONFIG_FILE} --output={OUTPUT}"
+    output = tmp_path / "test-output"
+    cmd = f"jade submit-jobs --dry-run -h {SLURM_HPC_CONFIG} {config_file} --output={output}"
     check_run_command(cmd)
